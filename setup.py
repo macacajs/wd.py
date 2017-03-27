4db1c03e80a9637c554a00c5e32c8c1641da4fc0
zenzhu <zic.zhu@gmail.com>
2017-03-27 09:43:21 +0800

    Add more keys

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='wd',
 
-    version='0.1.5',
+    version='0.1.6',
 
     description='Macaca Python Client',
     long_description=long_description,
